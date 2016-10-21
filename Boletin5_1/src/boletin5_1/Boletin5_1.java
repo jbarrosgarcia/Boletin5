20a4e23038a83545abe298e567455328094ab5c6
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-10-21 11:29:02 +0200

    primer commit

---
+++ Boletin5_1/src/boletin5_1/Boletin5_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_1;
+import java.util.Scanner;
+/**
+ *
+ * @author Jose Barros
+ */
+public class Boletin5_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado =new Scanner(System.in);
+        Numeros numero=new Numeros();
+        System.out.println("Introduzca el numero para saber si es positivo");
+        numero.setNumero(teclado.nextInt());
+        numero.comparar();
+    }
+    
+}
