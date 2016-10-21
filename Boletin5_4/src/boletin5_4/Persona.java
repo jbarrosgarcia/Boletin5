b836eda717892862bb9ccd29a1d86eb15adc7141
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-10-21 11:59:17 +0200

    primer commit

---
+++ Boletin5_4/src/boletin5_4/Persona.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_4;
+
+/**
+ *
+ * @author Jose Barros
+ */
+public class Persona {
+    private String nome;
+    private float peso;
+    
+    public Persona(){
+        this.nome="";
+        this.peso=0f;
+    }
+    
+    public Persona(String nome, float peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    
+}
