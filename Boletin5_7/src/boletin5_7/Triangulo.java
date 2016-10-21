9a8cefd48caffebf5c58f26ec41ee6e0e57873b5
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-10-21 12:35:03 +0200

    primer commit

---
+++ Boletin5_7/src/boletin5_7/Triangulo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_7;
+
+/**
+ *
+ * @author Jose Barros
+ */
+public class Triangulo {
+    
+}
