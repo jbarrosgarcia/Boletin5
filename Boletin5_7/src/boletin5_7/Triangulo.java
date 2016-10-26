0ed8a1401ed2e28311e91c5f5dd697afc7df85ee
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-10-26 09:29:03 +0200

    primer commit

--- Boletin5_7/src/boletin5_7/Triangulo.java
+++ Boletin5_7/src/boletin5_7/Triangulo.java
@@ -10,5 +10,34 @@ package boletin5_7;
  * @author Jose Barros
  */
 public class Triangulo {
-    
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Triangulo() {
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    public double calcArea(){
+        double area=(this.base*this.altura)/2;
+        return area;
+    }
 }
