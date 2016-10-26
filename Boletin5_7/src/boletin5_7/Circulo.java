fe7a1f28dace581d3d576ac5efd02156ea3cbac5
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-10-26 09:29:09 +0200

    primer commit

--- Boletin5_7/src/boletin5_7/Circulo.java
+++ Boletin5_7/src/boletin5_7/Circulo.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package boletin5_7;
-
+import java.math.*;
 /**
  *
  * @author Jose Barros
  */
 public class Circulo {
+    private double radio;
+    private final float PI=3.14f;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
     
+    public Circulo (){
+    
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    public double calcArea(){
+        double area=PI*Math.pow(radio, 2);
+        return area;
+    }
 }
