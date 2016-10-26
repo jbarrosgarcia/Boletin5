aa73ed5e4b44d25149174c9357574fdbf5f2b8f5
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-10-26 09:29:12 +0200

    primer commit

--- Boletin5_7/src/boletin5_7/Cadrado.java
+++ Boletin5_7/src/boletin5_7/Cadrado.java
@@ -10,5 +10,24 @@ package boletin5_7;
  * @author Jose Barros
  */
 public class Cadrado {
-    
+    private double lado;
+    public Cadrado() {
+    }
+
+    public Cadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+   public double calcArea(){
+     double area=lado*lado;
+     return area;
+   } 
+   
 }
